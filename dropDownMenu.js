dabae9bbb1beb162d89a44ccf7d54fbe738ea861
kwu70 <kwu70@gatech.edu>
2017-05-11 22:04:00 -0400

    Add files via upload

---
+++ dropDownMenu.js
@@ -0,0 +1,29 @@
+<attach event="onmouseover" handler="mouseover" />
+<attach event="onmouseout" handler="mouseout" />
+function mouseover()
+{
+  element.className += ' hover';
+  for(var x=0;x!=element.childNodes.length;++x)
+  {
+    if(element.childNodes[x].nodeType==1)
+    {
+      element.childNodes[x].className += 
+        ' parent_hover';
+    }
+  }
+}
+
+function mouseout()
+{
+  element.className =
+    element.className.replace(/ ?hover$/,'');
+  for(var x=0;x!=element.childNodes.length;++x)
+  {
+    if(element.childNodes[x].nodeType==1)
+    {
+      element.childNodes[x].className =
+      element.childNodes[x].className.replace
+        (/ ?parent_hover$/,'');
+    }
+  }
+}
